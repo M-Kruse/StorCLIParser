51eeede47f0daa324b996712e48ba97c3f3a2c74
M-Kruse <46699116+M-Kruse@users.noreply.github.com>
2019-08-28 16:19:01 -0400

    Update storcli_example.py

--- storcli_example.py
+++ storcli_example.py
@@ -1,35 +1,5 @@
 #!/usr/bin/env python3
-from subprocess import check_output
-
 from StorCLIParser import StorCLIParser
-
-# output = check_output(["/opt/MegaRAID/storcli/storcli64", "/c0", "show all"]).decode("utf-8")
-
-
-# #Create the controller dict
-# d = s.ctrl_info_dict(output)
-
-# #Loop through all keys and get their values
-# for key in d:
-# 	print(key, '->', d[key])
-
-
-# #Lookup a value with single key
-# print("\n")
-# print(d['Serial Number'])
-
-
-# output = check_output(["/opt/MegaRAID/storcli/storcli64", "/c0/e252/s1", "show", "all"]).decode("utf-8")
-
-# #print(output)
-# s = StorCLIParser()
-# #Create the controller dict
-# p = s.pd_info_dict(output)
-# #print(p)
-# #Loop through all keys and get their values
-# for key in p:
-# 	print(key, '->', p[key])
-
 
 s = StorCLIParser()
 
